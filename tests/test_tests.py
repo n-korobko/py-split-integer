4c86fe5e8a000001907a0285131c1283d850d1c3
Nikita <hkorobko@gmail.com>
2025-11-12 15:24:39 +0200

    Fixed Split integer

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,24 +1,84 @@
-from app.split_integer import split_integer
+import os
 
-def test_one_part():
-    assert split_integer(10, 1) == [10]
+import pytest
 
-def test_even_split():
-    assert split_integer(8, 2) == [4, 4]
+from app import split_integer
 
-def test_split_with_remainder():
-    assert split_integer(10, 3) == [3, 3, 4]
 
-def test_large_split():
-    result = split_integer(100, 9)
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
-    assert len(result) == 9
-    assert sum(result) == 100
+def path_to_main():
+    base_path = os.path.join("app", "test_split_integer.py")
+    return (
+        base_path if os.path.exists(base_path) else os.path.join(os.pardir, base_path)
+    )
 
-def test_almost_equal_parts():
-    result = split_integer(32, 6)
-    assert result == sorted(result)
-    assert max(result) - min(result) <= 1
-    assert len(result) == 6
-    assert sum(result) == 32
+
+def test_could_split_on_different_parts(monkeypatch):
+    def split_on_equal_part(value: int, number_of_parts: int):
+        return [value // number_of_parts] * number_of_parts
+
+    monkeypatch.setattr(split_integer, "split_integer", split_on_equal_part)
+
+    test_result = pytest.main(["app/test_split_integer.py"])
+    assert (
+        test_result.value == 1
+    ), "Function 'split_on_equal_part' shouldn't pass all tests"
+
+
+def test_only_last_number_is_incremented(monkeypatch):
+    def split_and_increment_the_last_number(value: int, number_of_parts: int):
+        result_list = [value // number_of_parts] * number_of_parts
+        if value % number_of_parts != 0:
+            result_list[-1] += 1
+        return result_list
+
+    monkeypatch.setattr(
+        split_integer, "split_integer", split_and_increment_the_last_number
+    )
+
+    test_result = pytest.main(["app/test_split_integer.py"])
+    assert (
+        test_result.value == 1
+    ), "Function 'split_and_increment_the_last_number' shouldn't pass all tests"
+
+
+def test_split_on_incorrect_parts(monkeypatch):
+    def split_on_incorrect_parts(value: int, number_of_parts: int):
+        result_list = [value // number_of_parts] * number_of_parts
+        for i in range(value % number_of_parts + 1):
+            result_list[-1 - i] += 1
+        result_list[0] -= 1
+        return result_list
+
+    monkeypatch.setattr(split_integer, "split_integer", split_on_incorrect_parts)
+
+    test_result = pytest.main(["app/test_split_integer.py"])
+    assert (
+        test_result.value == 1
+    ), "Function 'split_on_incorrect_parts' shouldn't pass all tests"
+
+
+def test_split_on_different_parts(monkeypatch):
+    def split_on_different_parts(value: int, number_of_parts: int):
+        result_list = [value // number_of_parts] * number_of_parts
+        for i in range(value % number_of_parts):
+            result_list[-1 - i] += 1
+        if value % number_of_parts == 0:
+            result_list[0] -= 1
+            result_list[-1] += 1
+        return result_list
+
+    monkeypatch.setattr(split_integer, "split_integer", split_on_different_parts)
+
+    test_result = pytest.main(["app/test_split_integer.py"])
+    assert (
+        test_result.value == 1
+    ), "Function 'split_on_different_parts' shouldn't pass all tests"
+
+
+def test_no_pass_in_code():
+    with open(path_to_main(), "r") as file:
+        tests_content = file.read()
+
+        assert (
+            "pass" not in tests_content
+        ), "You have to remove the 'pass' statement from your code if function is not empty"
